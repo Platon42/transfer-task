7a599ad29b622dce955a115de47730f1b1d4446f
kotol@icloud.com <kotol@icloud.com>
2019-09-01 23:21:21 +0300

    Initial

---
+++ src/main/java/mercy/digital/transfer/controller/Controller.java
@@ -0,0 +1,10 @@
+package mercy.digital.transfer.controller;
+
+import java.util.ArrayList;
+
+public class Controller {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("aaa");
+    }
+}
